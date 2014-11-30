6263598e51f9d77e5f90b1845e14306ac7d067b5
Edward <huangxx_2155@qq.com>
2014-11-29 17:02:05 -0800

    add a get_image.sh for basic Ubuntu image, Ubuntu with Hadoop and Ubuntu with Spark

--- get_image.sh
+++ get_image.sh
@@ -6,3 +6,15 @@ glance image-create \
 	--container-format bare \
 	--is-public True \
 	--copy-from http://uec-images.ubuntu.com/trusty/current/trusty-server-cloudimg-amd64-disk1.img
+glance image-create \
+        --name UbuntuTrustyVanilla2.4.1 \
+        --disk-format qcow2 \
+        --container-format bare \
+        --is-public True \
+        --copy-from http://sahara-files.mirantis.com/sahara-juno-vanilla-2.4.1-ubuntu-14.04.qcow2
+glance image-create \
+        --name UbuntuTrustySpark1.0 \
+        --disk-format qcow2 \
+        --container-format bare \
+        --is-public True \
+        --copy-from http://sahara-files.mirantis.com/sahara-juno-spark-1.0.0-ubuntu-14.04.qcow2
